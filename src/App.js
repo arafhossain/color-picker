58105bc5f7eb29eb4ae8701d617bca76f5afa332
Araf Hossain <arafh13@gmail.com>
2019-10-20 15:27:22 -0400

    Created ColorBox component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import Palette from './Palette'
-import seedColors from './seed'
+import seedColors from './seedColors'
 class App extends Component {
   render(){
     return (
       <div className="App">
-       <Palette />
+       <Palette {...seedColors[4]}/>
       </div>
     );
   }
--- src/Palette.js
+++ src/Palette.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
-
+import ColorBox from './ColorBox'
+import './Palette.css'
 class Palette extends Component {
   render(){
+    let colorBoxes = this.props.colors.map(color => (
+      <ColorBox background={color.color} name={color.name} />
+    ))
     return (
-      <div>
-        <h1>Palette!</h1>
+      <div className="Palette">
+        <div className="Palette-colors">
+        {colorBoxes}
+        </div>
       </div>
     )
   }
 }
+
+export default Palette;
