106dc83aa05a66529812ceda38c312c8fa6e002f
Araf Hossain <arafh13@gmail.com>
2019-10-20 19:57:27 -0400

    Added NavBar and Logo

--- src/Palette.js
+++ src/Palette.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
+import NavBar from './NavBar'
 import "./Palette.css";
 class Palette extends Component {
   constructor(props) {
@@ -20,15 +19,7 @@ class Palette extends Component {
     ));
     return (
       <div className="Palette">
-        <div className="slider">
-        <Slider
-          defaultValue={this.state.level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
-        </div>
+        <NavBar level={this.state.level} changeLevel={this.changeLevel}/>
         <div className="Palette-colors">{colorBoxes}</div>
       </div>
     );
