2df7ffce5a05f63264aab76a92dd9f8388fe7137
Araf Hossain <arafh13@gmail.com>
2019-10-21 12:30:38 -0400

    Fixed routes for different names

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./ColorHelpers";
 import {Switch, Route} from 'react-router-dom';
 class App extends Component {
+  getPalette(id){
+    return seedColors.find(function(palette){
+      return palette.id === id;
+    })
+  }
   render() {
     return (
       <Switch>
          <Route exact path="/" render={() => <h2>List goes here</h2>}/>
-        <Route exact path="/palette/:id"/>
+        <Route exact path="/palette/:id" render={routeProps => <Palette palette={generatePalette(this.getPalette(routeProps.match.params.id))}/>}/>
       </Switch>
       // <div className="App">
       //   <Palette palette={generatePalette(seedColors[4])} />
