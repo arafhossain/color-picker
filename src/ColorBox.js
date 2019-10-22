22598a544891d0092f6274bbf9bd0ac3b81232c6
Araf Hossain <arafh13@gmail.com>
2019-10-22 15:58:12 -0400

    More refactoring of ColorBox css

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -6,14 +6,14 @@ import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/styles";
 
 let styles = {
-  ColorBox:{
-    width: '20%',
-    height: props => props.showMore ? '25%' : '50%',
-    margin:'0 auto',
-    display: 'inline-block',
-    position: 'relative',
-    marginBottom: '-3.5px',
-    '&:hover button': {
+  ColorBox: {
+    width: "20%",
+    height: props => (props.showMore ? "25%" : "50%"),
+    margin: "0 auto",
+    display: "inline-block",
+    position: "relative",
+    marginBottom: "-3.5px",
+    "&:hover button": {
       opacity: 1
     }
   },
@@ -65,6 +65,66 @@ let styles = {
     border: "none",
     textDecoration: "none",
     opacity: 0
+  },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px"
+  },
+  copyOverlay: {
+    opacity: "0",
+    zIndex: "0",
+    width: "100%",
+    height: "100%",
+    transition: "transform 0.6s ease-in-out"
+  },
+  showOverlay: {
+    opacity: "1",
+    transform: "scale(50)",
+    zIndex: "10",
+    position: "absolute"
+  },
+  copyMessage: {
+    position: "fixed",
+    left: "0",
+    right: "0",
+    top: "0",
+    bottom: "0",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    fontSize: "4rem",
+    transform: "scale(0.1)",
+    opacity: "0",
+    color: "white",
+    "& h1": {
+      fontWeight: "400",
+      textShadow: "1px 2px black",
+      background: "rgba(255,255,255, 0.2)",
+      width: "100%",
+      textAlign: "center",
+      marginBottom: "0",
+      padding: "1rem",
+      textTransform: "uppercase"
+    },
+    "& p": {
+      fontSize: "2rem",
+      fontWeight: "200"
+    }
+  },
+  showMessage: {
+    opacity: "1",
+    transform: "scale(1)",
+    zIndex: "25",
+    transition: "all 0.4s ease-in-out",
+    transitionDelay: "0.3s"
   }
 };
 class ColorBox extends Component {
@@ -85,14 +145,18 @@ class ColorBox extends Component {
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`copy-overlay ${this.state.beingCopied && "show"}`}
+            className={`${classes.copyOverlay} ${this.state.beingCopied &&
+              classes.showOverlay}`}
           />
-          <div className={`copy-msg ${this.state.beingCopied && "show"}`}>
+          <div
+            className={`${classes.copyMessage} ${this.state.beingCopied &&
+              classes.showMessage}`}
+          >
             <h1>Copied!</h1>
             <p className={classes.copyText}>{this.props.background}</p>
           </div>
-          <div className="copy-container">
-            <div className="box-content">
+          <div>
+            <div className={classes.boxContent}>
               <span className={classes.nameText}>{name}</span>
             </div>
             <button className={classes.copyButton}>Copy</button>
