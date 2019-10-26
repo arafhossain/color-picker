46cc1b6230219e6f33942f3a85221c092f7ec8f2
Araf Hossain <arafh13@gmail.com>
2019-10-26 18:11:57 -0400

    Styled newColorBox

--- src/NewColorBox.js
+++ src/NewColorBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/styles";
+import DeleteIcon from '@material-ui/icons/Delete';
 
 let styles = {
   root: {
@@ -8,18 +9,41 @@ let styles = {
     margin: "0 auto",
     display: "inline-block",
     position: "relative",
-    marginBottom: "-3.5px"
+    marginBottom: "-3.5px",
+    '&:hover svg' : {
+      color: 'white',
+      transform: 'scale(1.2)'
+    }
+  }, boxContent : {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: 'flex',
+    justifyContent: 'space-between'
+  }, deleteIcon : {
+    color: 'rgba(0,0,0,0.5)',
+    transition: 'all 0.3s ease-in-out'
   }
 };
 
 class NewColorBox extends Component {
   render() {
+    let {classes} = this.props;
     return (
       <div
-        className={this.props.classes.root}
+        className={classes.root}
         style={{ backgroundColor: this.props.color }}
       >
-        {this.props.name}
+        <div className={classes.boxContent}>
+          <span>{this.props.name}</span>
+          <DeleteIcon className={classes.deleteIcon}/>
+        </div>
       </div>
     );
   }
