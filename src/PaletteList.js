6cd4114f76fc622965304d51a4e534b0760630a9
Araf Hossain <arafh13@gmail.com>
2019-10-30 19:37:13 -0400

    Animated palette deletion

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import MiniPalette from "./MiniPalette";
 import { withStyles } from "@material-ui/styles";
-import styles from './styles/PaletteListStyles'
-import {Link} from 'react-router-dom';
+import styles from "./styles/PaletteListStyles";
+import { Link } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 class PaletteList extends Component {
-  goToPalette(id){
-    this.props.history.push(`/palette/${id}`)
+  goToPalette(id) {
+    this.props.history.push(`/palette/${id}`);
   }
   render() {
     let { classes } = this.props;
@@ -16,11 +17,18 @@ class PaletteList extends Component {
             <h2 className={classes.title}>Color Picker</h2>
             <Link to="/palette/new">Create Palette</Link>
           </nav>
-          <div className={classes.palettes}>
-            {this.props.palettes.map(palette => (
-              <MiniPalette {...palette} removePalette={this.props.removePalette} handleClick={() => this.goToPalette(palette.id)} key={palette.id}/>
-            ))}
-          </div>
+            <TransitionGroup className={classes.palettes}>
+              {this.props.palettes.map(palette => (
+                <CSSTransition key={palette.id} classNames="fade" timeout={500}>
+                  <MiniPalette
+                    {...palette}
+                    removePalette={this.props.removePalette}
+                    handleClick={() => this.goToPalette(palette.id)}
+                    key={palette.id}
+                  />
+                </CSSTransition>
+              ))}
+            </TransitionGroup>
         </div>
       </div>
     );
